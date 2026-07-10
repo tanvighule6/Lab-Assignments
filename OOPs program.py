717224142447c5ee305bd38767e8b0808e31f9ba
Tanvi Ghule <tanvighule2007@gmail.com>
2026-07-10 23:39:15 +0530

    Initial commit

---
+++ OOPs program.py
@@ -0,0 +1,28 @@
+class BankAccount:
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print("Deposited ₹", amount)
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            print("Withdrawn ₹", amount)
+        else:
+            print("Insufficient Balance")
+
+    def display(self):
+        print("\n----- Account Details -----")
+        print("Account Holder :", self.name)
+        print("Current Balance: ₹", self.balance)
+
+
+account = BankAccount("Tanvi", 5000)
+
+account.display()
+account.deposit(2000)
+account.withdraw(1000)
+account.display()
